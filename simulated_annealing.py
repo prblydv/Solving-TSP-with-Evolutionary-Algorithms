5a1a264296b4eab0299688198318257ba2717064
Prabal pratap singh <yadavsk731@gmail.com>
2025-09-18 12:17:14 +0530

    Cleaned up simulated_annealing.py

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -16,6 +16,7 @@ class SimulatedAnnealingTSP:
         current_solution = random.sample(self.tsp_problem.cities, len(self.tsp_problem.cities))
         current_route = [city.id for city in current_solution]
         current_distance = self.tsp_problem.total_distance(current_route)
+
         best_solution, best_distance = current_route, current_distance
         temperature = self.initial_temp
 
