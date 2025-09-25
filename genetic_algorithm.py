ac484a5f168ad23323f185804527dd9ea2c82053
Prabal pratap singh <yadavsk731@gmail.com>
2025-09-25 09:39:00 +0530

    Adjusted spacing around operators in genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -52,7 +52,7 @@ class GeneticAlgorithmTSP:
             next_population.append(offspring)
 
         self.population = next_population
-
+ 
     def run(self):
         """Runs the genetic algorithm."""
         best_tour = min(self.population, key=lambda tour: self.tsp_problem.total_distance(tour))
