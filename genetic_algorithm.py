8ee3ea2914b188302fc7fa3a1a01bcb7645774b6
Prabal pratap singh <yadavsk731@gmail.com>
2025-10-25 09:47:02 +0100

    Applied PEP8 formatting to genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -55,7 +55,7 @@ class GeneticAlgorithmTSP:
             next_population.append(offspring)
 
         self.population = next_population
- 
+  
     def run(self):
         """Runs the genetic algorithm."""
         best_tour = min(self.population, key=lambda tour: self.tsp_problem.total_distance(tour))
