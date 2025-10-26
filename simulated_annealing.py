4437a2ce6a5c4b153bc3b0776c512519fa5b1954
Prabal pratap singh <yadavsk731@gmail.com>
2025-10-26 09:26:45 +0000

    Updated function spacing in simulated_annealing.py

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -34,7 +34,7 @@ class SimulatedAnnealingTSP:
                 current_route = new_route
                 current_distance = new_distance
                 if current_distance < best_distance:
-
+ 
                     best_solution, best_distance = current_route, current_distance
 
             temperature *= (1 - self.cooling_rate)
