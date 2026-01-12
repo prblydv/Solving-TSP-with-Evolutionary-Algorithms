d06f0f23c37eb7b043bea5c8db7630b80ade27d5
Prabal pratap singh <yadavsk731@gmail.com>
2026-01-12 09:31:47 +0000

    Cleaned up option2.py

--- option2.py
+++ option2.py
@@ -79,7 +79,7 @@ class TSPProblem:
     def pseudo_euclidean_distance(self, city1, city2):
         dx = city1.x - city2.x
         dy = city1.y - city2.y
-        return round(math.sqrt((dx ** 2 + dy ** 2) / 10.0))
+        return round(math.sqrt((dx ** 2 + dy ** 2) / 10.0)) 
 
     def total_distance(self, path):
         distance = 0
