175b1666f1e866f9a96237370928b2a648cc5bca
Prabal pratap singh <yadavsk731@gmail.com>
2026-02-06 09:41:05 +0000

    Refactor whitespace in simulated_annealing.py

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -25,6 +25,7 @@ class SimulatedAnnealingTSP:
         """Runs the Simulated Annealing algorithm."""
  
 
+
         current_solution = random.sample(self.tsp_problem.cities, len(self.tsp_problem.cities))
         current_route = [city.id for city in current_solution]
 
