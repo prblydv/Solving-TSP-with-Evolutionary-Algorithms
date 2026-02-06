1ce12dc49ea1642d5a1c83fac1f8099afe688640
Prabal pratap singh <yadavsk731@gmail.com>
2026-02-06 09:35:33 +0000

    Brought tsp_problem.py in line with codebase style

--- tsp_problem.py
+++ tsp_problem.py
@@ -25,6 +25,7 @@ class TSPProblem:
             city1 = self.cities[path[i] - 1]
 
 
+
             city2 = self.cities[path[i + 1] - 1]
             distance += self.pseudo_euclidean_distance(city1, city2)
         return distance 
